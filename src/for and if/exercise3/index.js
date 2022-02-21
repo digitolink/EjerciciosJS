cbc0c5511413d0735b2fbbc950f64fb3de5886ea
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 12:38:40 +0100

    ejercicio tres hecho

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -1,7 +1,12 @@
 const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
+let counter=0;
 
-// Put your code here
+for (item of numbers){
+    sum+=item;
+    counter+=1;
+}
+average=sum/counter;
 
-console.log(average)
+console.log(average);
