9d8b62197136f6b72b11c68817f2413cf7a41aa9
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:45:10 +0100

    ejercicio 10 hecho

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,3 +1,12 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 
-// Put your code here
+for(let i=1;i<=numbers.length;i++){
+    for(let j=0;j<numbers.length;j++){
+            if(numbers[j]<numbers[j+1]){
+                let temp=numbers[j];
+                numbers[j]=numbers[j+1];
+                numbers[j+1]=temp;
+            }
+    }
+}
+console.log(numbers);
