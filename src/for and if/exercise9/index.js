a5fbfeb4c00a419b34cda071e07c916230f8823d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:35:14 +0100

    ejercicio 9 hecho

--- src/for and if/exercise9/index.js
+++ src/for and if/exercise9/index.js
@@ -2,6 +2,7 @@ let numbers = [0,1,2,3,4,5,6,7,8,9]
 
 for ( let left of numbers) {
     for (let right of numbers) {
-        // Put your code here
+        console.log(left*right);
     }
+    console.log("\n");
 }
