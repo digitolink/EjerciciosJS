5868084fb53e32ce821975a979fbb6dca8bd4358
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 12:34:48 +0100

    ejercicio dos hecho

--- src/for and if/exercise2/index.js
+++ src/for and if/exercise2/index.js
@@ -1,8 +1,8 @@
 const numbers = [0,1,2,4,5,9,3,6,7,8];
-
+let counter=0;
 // Put your code here
 for (let item of numbers) {
-    // Put your code here
+    counter+=1;
 }
 
-console.log(/* Put your code here */)
+console.log(counter);
