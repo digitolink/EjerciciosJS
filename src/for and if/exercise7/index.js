d0f687c37d6cbd644a6d2ebf3f52f7f583feaf97
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:16:41 +0100

    ejercicio 7 hecho

--- src/for and if/exercise7/index.js
+++ src/for and if/exercise7/index.js
@@ -1,4 +1,7 @@
 const numbers = ["Frodo","Gandalf","Turin","Sauron","Saruman","Bilbo"];
 
-// Put your code here
+//nota: el elemento 3 tiene indice 2
+for(let index=2;index<=4;index++){
+    console.log(numbers[index]);
+}
 
