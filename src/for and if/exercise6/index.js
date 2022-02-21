346bd427e9bc5e2892664514c905f5ab0aead4dd
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:09:34 +0100

    ejercicio 6 hecho

--- src/for and if/exercise6/index.js
+++ src/for and if/exercise6/index.js
@@ -1,9 +1,16 @@
 const mix = [undefined,22,"1",4,3,"5",7,NaN,8,"9","12",11,null];
+let strings = [];
+let numbers = [];
 
 for (let item of mix) {
-    let strings = [];
-    let numbers = [];
-    // Put your code here
+    if (typeof(item)==="number" && !isNaN(item)){
+            numbers.push(item);
+    }
+    else if(typeof(item)==="string"){
+        strings.push(item);
+    }
+
+
 }
 
-console.log("Strings:",strings,"Numbers:",numbers)
+console.log("Strings:",strings,"Numbers:",numbers);
