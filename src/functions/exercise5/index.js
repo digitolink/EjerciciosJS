6e70f3a62f628908efbafbd640cf8bb7471a0a5c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 12:04:18 +0100

    ejercicio 5 hecho

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,7 +1,19 @@
 const numbers = [11,9,13,12]
 
-// Put your code here
+function minorInRange(pos1,pos2,arraynum){
+    let menor=arraynum[pos1];
+    for(let i=pos1;i<pos2;i++){
+        if(arraynum[i] < arraynum[i+1]){
+            menor =arraynum[i];    
+         }
+
+    }
+    if(arraynum[pos2]<menor){
+        menor=arraynum[pos2];
+    }
+    return menor;
+}
 
 console.log(minorInRange(0,3,numbers));
 console.log(minorInRange(2,3,numbers));
-console.log(minorInRange(1,3),numbers);
+console.log(minorInRange(1,3,numbers));
