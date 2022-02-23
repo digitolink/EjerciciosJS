1552242420110e3cfcd9333770f6a3e72fc8a7e8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 09:01:07 +0100

    ejercicio 4 hecho

--- src/functions/exercise4/index.js
+++ src/functions/exercise4/index.js
@@ -1,6 +1,11 @@
 const numbers = [11,12,13,14]
 
-// Put your code here
+function swap(pos1,pos2,arraynum){
+    const temp=arraynum[pos1];
+    arraynum[pos1]=arraynum[pos2];
+    arraynum[pos2]=temp;
+
+}
 
 swap(0,3,numbers);
 console.log(numbers);
