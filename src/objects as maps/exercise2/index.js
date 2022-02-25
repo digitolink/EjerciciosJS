0f39e00591443b103257395741eccf5d22d45855
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-25 13:47:24 +0100

    ejercicio 2 objetos a medio hacer

--- src/objects as maps/exercise2/index.js
+++ src/objects as maps/exercise2/index.js
@@ -21,6 +21,47 @@ const chrono = [
     },
 ]
 
-// Put your code here
 
+//funcion para intercambiar los valores entre 
+//dos posiciones cualesquiera de un array
+function swap(pos1,pos2,arraynum){
+    const temporal=arraynum[pos1];
+    arraynum[pos1]=arraynum[pos2];
+    arraynum[pos2]=temporal;
+}
+
+//funcion para encontrar la posicion del objeto persona que 
+//tenga menor tiempo
+//
+function menorTiempoYSuPosicion(arrayDeObjetos){
+    let menor=arrayDeObjetos[0].time;
+    let posmenor=0;
+    for(let i=0;i<arrayDeObjetos[i]!=undefined;i++){
+        if(arrayDeObjetos[i].time < arrayDeObjetos[i+1].time){
+            menor =arrayDeObjetos[i];
+            posmenor=i;    
+         }
+
+    }
+    return posmenor;
+}
+
+
+/* /* Funcion que ordena los participantes 
+por su tiempo, de la persona mas rapida
+a la mas lenta.
+algoritmo: 
+-encontramos la posicion del menor del array, 
+con i=0 hasta que el objeto en la posicion i
+sea undefined.
+-la intercambiamos con la primera posicion.
+-repetimos 
+*/
+    for(let i=0;chrono[i]!=undefined;i++){
+        let posicionMenor=menorTiempoYSuPosicion(chrono);
+        swap(posicionMenor, i, chrono);
+    }
+
+
+//
 console.log(JSON.stringify(chrono))
