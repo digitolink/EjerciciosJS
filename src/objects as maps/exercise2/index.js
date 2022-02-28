676fc6c3fb204dd991cd3bb975f7c85665dac47f
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-28 12:10:39 +0100

    nuevo avance

--- src/objects as maps/exercise2/index.js
+++ src/objects as maps/exercise2/index.js
@@ -24,44 +24,54 @@ const chrono = [
 
 //funcion para intercambiar los valores entre 
 //dos posiciones cualesquiera de un array
+
+/**
+ * 
+ * @param {number} pos1 
+ * @param {number} pos2 
+ * @param {Array} arraynum 
+ */
 function swap(pos1,pos2,arraynum){
     const temporal=arraynum[pos1];
     arraynum[pos1]=arraynum[pos2];
     arraynum[pos2]=temporal;
 }
 
-//funcion para encontrar la posicion del objeto persona que 
-//tenga menor tiempo
-//
-function menorTiempoYSuPosicion(arrayDeObjetos){
-    let menor=arrayDeObjetos[0].time;
-    let posmenor=0;
-    for(let i=0;i<arrayDeObjetos[i]!=undefined;i++){
-        if(arrayDeObjetos[i].time < arrayDeObjetos[i+1].time){
-            menor =arrayDeObjetos[i];
-            posmenor=i;    
-         }
-
-    }
-    return posmenor;
-}
-
-
-/* /* Funcion que ordena los participantes 
-por su tiempo, de la persona mas rapida
-a la mas lenta.
+/*funcion para encontrar la posicion del objeto persona que 
+tenga menor tiempo
 algoritmo: 
 -encontramos la posicion del menor del array, 
 con i=0 hasta que el objeto en la posicion i
 sea undefined.
--la intercambiamos con la primera posicion.
+-la intercambiamos con la posicion i.
 -repetimos 
 */
-    for(let i=0;chrono[i]!=undefined;i++){
-        let posicionMenor=menorTiempoYSuPosicion(chrono);
-        swap(posicionMenor, i, chrono);
+/**
+ * 
+ * @param {Array} arrayDeObjetos 
+ * @returns {number} posmenor
+ */
+function intercambiaMenor(arrayDeObjetos){
+    let menor=arrayDeObjetos[0].time;
+    //let posmenor=0;
+    for(let i=0;arrayDeObjetos[i+1]!=undefined;i++){
+        objetoActual=arrayDeObjetos[i];
+        objetoSiguiente=arrayDeObjetos[i+1];
+        objetoFinal=arrayDeObjetos[arrayDeObjetos.length-1];
+
+        if(objetoActual.time < objetoSiguiente.time){
+            swap(i,i+1, arrayDeObjetos);
+            menor =arrayDeObjetos[i];
+            //posmenor=i;    
+         }
+
+         if(objetoFinal.time < menor){
+            swap(arrayDeObjetos.length-1, menor, arrayDeObjetos);
+            //posmenor=arrayDeObjetos.length;    
+         }
     }
 
+}
 
-//
+
 console.log(JSON.stringify(chrono))
